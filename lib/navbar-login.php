6421e5985dfa67767e397ce3c934001bfa78d32e
budistwn15 <budi.setiawan112001@gmail.com>
2021-10-15 01:37:14 +0700

    Navbar-Login

---
+++ lib/navbar-login.php
@@ -0,0 +1,9 @@
+<section class="navbar-login">
+    <nav class="navbar navbar-expand-sm navbar-light bg-white">
+        <div class="container">
+            <a class="navbar-brand m-auto" href="index.php">
+                <img src="assets/images/logo/logo.png" alt="">
+            </a>
+        </div>
+    </nav>
+</section>
