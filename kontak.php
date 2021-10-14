0d3f5d4be01365a666ec5b1651c7c486c605d3b4
budistwn15 <budi.setiawan112001@gmail.com>
2021-10-15 01:51:52 +0700

    Kontak

---
+++ kontak.php
@@ -0,0 +1,39 @@
+<section class="bantuan bg-primary mb-5" style="border-bottom-left-radius: 60px;">
+    <div class="container">
+        <div class="row p-5">
+            <div class="col-md-8 m-auto">
+                <h3 class="text-white text-center fw-bold">Hei, Kamu!</h3>
+                <p class="lead text-center text-white">Apabila mengalami kesulitan silahkan hubungi kami</p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<div class="container">
+    <div class="row mb-4" style="margin-top: -90px;">
+        <div class="col-md-6 offset-6">
+            <div class="card p-4 bg-white shadow border-0" style="border-top-left-radius:40px">
+                <h5 class="mb-3">Hubungi Kami</h5>
+                <form action="" method="POST">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="" id="" class="form-control" placeholder="Masukkan Nama">
+                        <label for="">Name</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="email" name="" id="" class="form-control" placeholder="Masukkan Email">
+                        <label for="">Email</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="text" name="" id="" class="form-control" placeholder="Masukkan Subjek">
+                        <label for="">Subjek</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <textarea id="summernote" name="" placeholder="Masukkan Pesan" class="form-control"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Kirim</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
